0e41c869c7772711324b07e4329dfbd1389920eb
Md Ashiqur Rahman Pranto <pranto.gpi@gmail.com>
2025-12-08 18:47:40 +0600

    created booking delete api

--- index.js
+++ index.js
@@ -186,6 +186,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete("/booking/:id", verifyJWT, async(req, res) => {
+            const {id} = req.params;
+            const query = {_id: new ObjectId(id)};
+            const result = await bookingsCollection.deleteOne(query);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db('admin').command({ ping: 1 });
         console.log('Pinged your deployment. You successfully connected to MongoDB!');
